f819e69adb0e803f4bb8c9042eb5d27e8de92a4a
Lonly-Lokly <102942172+Lonly-Lokly@users.noreply.github.com>
2025-06-11 02:31:36 +0300

    ассеты, шрифт, немного стилей

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './components/StartPage/StartPage';
 import UploadPage from './Pages/UploadPage/UploadPage';
 import SurveyPage from './Pages/SurveyPage/SurveyPage';
@@ -7,10 +7,6 @@ import ReportPage from './Pages/ReportPage/ReportPage';
 function App() {
     return (
         <BrowserRouter>
-            <nav>
-                <Link to="/">Главная</Link> | <Link to="/upload">Загрузка</Link> |{' '}
-                <Link to="/survey">Опрос</Link> | <Link to="/report">Результат</Link>
-            </nav>
             <Routes>
                 <Route path="/" element={<StartPage />} />
                 <Route path="/upload" element={<UploadPage />} />
